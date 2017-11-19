b33168a202298c38383d0eaa170ee41c25a82196
Luis <valdez.jds.16@gmail.com>
2017-11-19 19:41:21 +0100

    encodage fini

--- main.c
+++ main.c
@@ -3,13 +3,13 @@
 #include <string.h>
 #include <tchar.h>
 
-
 int main(int argc, char ** argv)
 {
   char* pathOfFile = "D:\\Users\\Luis Valdez\\Documents\\ESGI\\Annee 2017 - 2018\\Langage C\\Projet\\seven.txt";
   char* pathOfKey = "D:\\Users\\Luis Valdez\\Documents\\ESGI\\Annee 2017 - 2018\\Langage C\\Projet\\key.txt";
   char* stringUserBinary;
-  char stringOfKey[36];
+  char stringOfKey[36] = "\0";
+  char * stringX;
   char hexaTemp[3] = "00\0", tempChar, binaryTemp[9] = "00000000\0";
   FILE* userFile = NULL;
   int i, y, tempHexa;
@@ -22,22 +22,19 @@ int main(int argc, char ** argv)
   /******************************************** lecture et conversion en binaire du fichier ********************************************/
   if(userFile != NULL){
 
-    printf("good job (ligne 20)\n");
+    printf("good job (ligne 25)\n");
 
     fseek(userFile, 0, SEEK_END);
     sizeOfFile = ftell (userFile);
-    printf("la taille du userFile est %d (ligne 24)\n",sizeOfFile);
-
+    printf("la taille du userFile est %d (ligne 29)\n",sizeOfFile);
     stringUserBinary = (char*)malloc(sizeof(char)*sizeOfFile*8 + 2);
     stringUserBinary[0] = '\0';
     if(stringUserBinary!= NULL){
-      printf("all Good (ligne 29)\n");
-      printf("phrase à coder (ligne 30) : ");
+      printf("all Good (ligne 34)\n");
+      printf("phrase a coder (ligne 35) : \n");
       fseek (userFile , 0 , SEEK_SET );
-      printf("%s\n", stringUserBinary);
 
       while(currentChar != EOF){
-
         currentChar = fgetc(userFile);
         if(currentChar == EOF) break;
         printf("%c", currentChar);
@@ -58,22 +55,16 @@ int main(int argc, char ** argv)
             }
           }
         }
-        fclose(userFile);
         strcat(stringUserBinary, binaryTemp);
       }
+      fclose(userFile);
       //printf("\n%s\n", stringUserBinary);
       i = 0;
       printf("\n******** conversion binaire ********\n");
-      while(stringUserBinary[i] != '\0'){
-        if(i % 8 == 0 && i != 0) printf(" ");
-        printf("%c", stringUserBinary[i]);
-        i++;
-      }
-      printf("\n");
+      printf("%s\n", stringUserBinary);
 
 
       userFile = fopen(pathOfKey, "r");
-
       if(userFile != NULL){
 
         /******************************************** vérification de la clé et enregistrement de celle-ci ********************************************/
@@ -104,30 +95,51 @@ int main(int argc, char ** argv)
         stringOfKey[0] = '\0';
         fseek (userFile , 5 , SEEK_SET );
         i = 0;
-        while(currentChar != ']' || currentChar == EOF){
+        currentChar = '\0';
+        while(currentChar != ']' || currentChar != EOF){
           currentChar = fgetc(userFile);
           if(currentChar == ']') break;
-          printf("%c ",currentChar);
-          stringOfKey[i] == currentChar;
+          if(currentChar == ' ')continue;
+          stringOfKey[i] = currentChar;
           i++;
         }
         stringOfKey[35] = '\0';
-        printf("\nchaine : %s\n", stringOfKey);
+        printf("\nkey(ligne 113) : %s, taille : %d\n", stringOfKey, strlen(stringOfKey));
+        close(userFile);
+
         /******************************************** encodage avec la matrice G4 ********************************************/
+        stringX = malloc(strlen(stringUserBinary) *2 + 1);
+        if(stringX != NULL){
+          stringX[0] = '\0';
+          count = 0;
+          for(i = 0; i < strlen(stringUserBinary);){
+            //  if(i % 4 == 0) y = 0;
+            for(y = 0; y < 8; y++){
+              if(((stringUserBinary[i] - '0' && stringOfKey[y] - '0') + (stringUserBinary[i + 1] - '0' && stringOfKey[y + 8] - '0') + (stringUserBinary[i + 2] - '0' && stringOfKey[y + 8 * 2] - '0') + (stringUserBinary[i + 3] - '0' && stringOfKey[y + 8 * 3] - '0')) % 2){
+                stringX[count] = '1';
+              }
+              else stringX[count] = '0';
+              count++;
+            }
+            i += 4;
+          }
+          stringX[count] = '\0';
+          printf("\nencoded binary : %s\n", stringX);
 
-          i = 0;
-          /*while(stringUserBinary[i] != '\0'){
 
-          }*/
+          /******************************************** conversion du binaire encodé en ASCII ********************************************/
+          
 
+        }
+        else return 0;
 
-          close(userFile);
+        free(stringX);
+        free(stringUserBinary);
+
       }
-
       else return 0;
 
 
-      free(stringUserBinary);
     }
     else return 0;
 
